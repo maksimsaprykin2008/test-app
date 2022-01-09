6ae556a0f32f0d8e19404be60b484a41d56a0383
Maxim Saprykin <samaksim208@mail.ru>
2022-01-09 14:42:30 +0300

    Subtext

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,16 +1,19 @@
 import Kolba from './Kolba';
 
 const FrontPage = () => {
-  return <div className="container">
-    <div className="cork"></div>
-    <h1 className="header">
-      Chem
-    </h1>
-    <Kolba />
-    <h1 className="header">
-      stry
-    </h1>
-  </div>
+  return  <div className="container">
+    <div className="subcontainer">
+      <div className="cork"></div>
+      <h1 className="header">
+        Chem
+      </h1>
+      <Kolba />
+      <h1 className="header">
+        stry
+      </h1>
+      </div>
+      <div className="subtext">Химия</div>
+    </div>
 }
 
 export default FrontPage;
