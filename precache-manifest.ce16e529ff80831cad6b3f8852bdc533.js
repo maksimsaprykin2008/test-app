d7216eb30678b5431297dc24cb995a10957a3260
Maxim Saprykin <samaksim208@mail.ru>
2022-01-10 14:12:22 +0300

    Updates

---
+++ precache-manifest.ce16e529ff80831cad6b3f8852bdc533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073f4504c9c0ac324293448afb9fa30e",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "4beba57fdf1516128b2d",
+    "url": "/test-app/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "02f2325bb81f6e700238",
+    "url": "/test-app/static/css/main.8bfb1c64.chunk.css"
+  },
+  {
+    "revision": "4beba57fdf1516128b2d",
+    "url": "/test-app/static/js/2.c9abe156.chunk.js"
+  },
+  {
+    "revision": "9455e3aaf5f0bac8149d",
+    "url": "/test-app/static/js/3.9e436de9.chunk.js"
+  },
+  {
+    "revision": "02f2325bb81f6e700238",
+    "url": "/test-app/static/js/main.dacad0ff.chunk.js"
+  },
+  {
+    "revision": "14f2bf8d0e461b2ae2c5",
+    "url": "/test-app/static/js/runtime~main.e9c19458.js"
+  }
+]);
